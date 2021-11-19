00053d7919d0912e87e9173531500ccbda1e67dc
Nhat Quang Ho <nhat-quang.ho@etu.univ-grenoble-alpes.fr>
2021-11-19 15:26:35 +0100

    responsives pages

--- pages/listBooks.php
+++ pages/listBooks.php
@@ -7,6 +7,7 @@ session_start();
 <head>
     <link rel="stylesheet" type="text/css" href="/css/mainpage.css">
     <title>YOUR BOOKS</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 </head>
 
 <?php
--- pages/report.php
+++ pages/report.php
@@ -3,8 +3,9 @@
 <html>
 
 <head>
-    <title>REPORT</title>
     <link rel="stylesheet" type="text/css" href="/css/mainpage.css">
+    <title>REPORT</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 </head>
 
 <body>
