18bf989a6ad35799a05ae6c4af9b788e3f4f3155
Nhat Quang Ho <nhat-quang.ho@etu.univ-grenoble-alpes.fr>
2021-11-23 12:01:09 +0100

    fix bug create account

--- modules/accounts/creatAcc.php
+++ modules/accounts/creatAcc.php
@@ -58,63 +58,73 @@ if(isset($_POST['creatacc']) && ($nameErr=="" && $usrnameErr=="" && $emailErr ==
     #create user account and its book table
     $name = $_POST['name'];
     $username = $_POST['username'];
+    $email = $_POST["email"];
     $pass = generateRandomString(8);
     $language = $_POST['language'];
-    $existacc = mysql_query("SELECT user_name FROM login_user where user_name='$username'") 
+    $existusername = mysql_query("SELECT user_name FROM login_user where user_name='$username'") 
         or die('<script>console.log("Error SQL : ")' . mysql_error() . '</script>');
-    if(mysql_fetch_assoc($existacc)){
-        die('<script>alert("Username existed, please choose another one!");</script>');
+    $existemail = mysql_query("SELECT email FROM login_user where email='$email'") 
+        or die('<script>console.log("Error SQL : ")' . mysql_error() . '</script>');
+    if(mysql_fetch_assoc($existusername)){
+        echo '<script>alert("Username existed, please choose another one!");</script>';
         echo '<script>console.log("Username existed")</script>';
+        echo '<script type="text/javascript">setTimeout(function(){window.top.location="/pages/login.php"} , 0);</script>';
     }
-    $adduser = mysql_query("INSERT INTO login_user(name,user_name,password,language) VALUES('$name','$username','$pass','$language')") 
+    elseif(mysql_fetch_assoc($existemail)){
+        echo '<script>alert("Email is already associated with an account, please use another one!");</script>';
+        echo '<script>console.log("Email existed")</script>';
+        echo '<script type="text/javascript">setTimeout(function(){window.top.location="/pages/login.php"} , 0);</script>';
+    }
+    else {
+      $adduser = mysql_query("INSERT INTO login_user(name,user_name,email,password,language) VALUES('$name','$username','$email','$pass','$language')") 
         or die('<script>console.log("Error SQL : ")' . mysql_error() . '</script>');
 
-    $creattab = mysql_query("CREATE TABLE $username (
-        num INT NOT NULL,
-        isbn VARCHAR(30) NOT NULL,
-        title VARCHAR(50) NOT NULL,
-        category VARCHAR(30) NOT NULL,
-        year YEAR,
-        edition VARCHAR(30),
-        authors VARCHAR(50),
-        summary TEXT,
-        last_modification DATETIME NOT NULL,
-        PRIMARY KEY ( `isbn` )
-        )") or die('<script>console.log("Error SQL : ")' . mysql_error() . '</script>');
-    
+      $creattab = mysql_query("CREATE TABLE $username (
+          num INT NOT NULL,
+          isbn VARCHAR(30) NOT NULL,
+          title VARCHAR(50) NOT NULL,
+          category VARCHAR(30) NOT NULL,
+          year YEAR,
+          edition VARCHAR(30),
+          authors VARCHAR(50),
+          summary TEXT,
+          last_modification DATETIME NOT NULL,
+          PRIMARY KEY ( `isbn` )
+          )") or die('<script>console.log("Error SQL : ")' . mysql_error() . '</script>');
+      
 
-    #send password via email
-    ini_set("SMTP",$_ENV['SMTP_URL']);
-    ini_set("smtp_port",$_ENV['SMTP_PORT']);
-    $to = $_POST['email'];
-    $subject = 'bibliomaison.free.fr PASSWORD';
-    $message = "Hello " . $_POST['name'] . ","
-                . "<br>You can now log in to http://bibliomaison.free.fr/pages/login.php :"
-                . "<br><br>Username:  " . $_POST['username']
-                . "<br>Password:  " . $pass
-                . "<br><br>You can change your password later."
-                . "<br> If you have any question, please contact bibliomaison@free.fr"
-                . "<br><br>ENJOY! :)"
-                . "<br>BIBLIOMAISON Team";
-    $headers = "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-    $headers .= 'From:' . $_ENV['FROM_NOREPLY'] . "\r\n";
-    $headers .= 'Cc: ' . "\r\n";
-    $headers .= 'Bcc: ' . "\r\n";
-    if (mail($to, $subject, $message, $headers)) {
-        echo '<script>
-                console.log("message sent");
-            </script>';
-    } 
-    else {
-        echo '<script>
-                console.log("failed to sent message");
-            </script>';
+      #send password via email
+      ini_set("SMTP",$_ENV['SMTP_URL']);
+      ini_set("smtp_port",$_ENV['SMTP_PORT']);
+      $to = $_POST['email'];
+      $subject = 'bibliomaison.free.fr PASSWORD';
+      $message = "Hello " . $_POST['name'] . ","
+                  . "<br>You can now log in to http://bibliomaison.free.fr/pages/login.php :"
+                  . "<br><br>Username:  " . $_POST['username']
+                  . "<br>Password:  " . $pass
+                  . "<br><br>You can change your password later."
+                  . "<br> If you have any question, please contact bibliomaison@free.fr"
+                  . "<br><br>ENJOY! :)"
+                  . "<br>BIBLIOMAISON Team";
+      $headers = "MIME-Version: 1.0" . "\r\n";
+      $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+      $headers .= 'From:' . $_ENV['FROM_NOREPLY'] . "\r\n";
+      $headers .= 'Cc: ' . "\r\n";
+      $headers .= 'Bcc: ' . "\r\n";
+      if (mail($to, $subject, $message, $headers)) {
+          echo '<script>
+                  console.log("message sent");
+              </script>';
+      } 
+      else {
+          echo '<script>
+                  console.log("failed to sent message");
+              </script>';
+      }
+
+      echo '<script>alert("Your account is successfully created, please verify your email (as well your SPAM) to get your password");</script>';
+      echo '<script>console.log("User created")</script>';
+      echo '<script type="text/javascript">setTimeout(function(){window.top.location="/pages/login.php"} , 0);</script>';
     }
-
-    echo '<script>alert("Your account is successfully created, please verify your email (as well your SPAM) to get your password");</script>';
-    echo '<script>console.log("User created")</script>';
-    echo '<script type="text/javascript">setTimeout(function(){window.top.location="/pages/login.php"} , 0);</script>';
-
 }
 ?>
