9536ab8aa6e0dc5298a1510edfe39bcc05f8857e
Nhat Quang Ho <nhat-quang.ho@etu.univ-grenoble-alpes.fr>
2021-11-24 02:50:48 +0100

    mini update

--- include/start_page.php
+++ include/start_page.php
@@ -2,7 +2,7 @@
 <center>
 <h2>Choose your language / Chọn ngôn ngữ:</h2>
 <br>
-<h3><a href="/pages/login.php">ENGLISH</a></h3>
+<h3><a href="/pages/login.php">ENGLISH / TIẾNG ANH</a></h3>
 <br>
-<h3><a href="/vn/pages/login.php">VIETNAMESE</a></h3>
+<h3><a href="/vn/pages/login.php">VIETNAMESE / TIẾNG VIỆT</a></h3>
 </center>
